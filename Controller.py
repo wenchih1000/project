5bf41c404825c0a290cd2ac46e2743361f4f30b3
wenchih1000 <wenchih1000@gmail.com>
2025-10-13 09:11:10 +0800

    add some help description.

--- Controller.py
+++ Controller.py
@@ -1,5 +1,5 @@
 from Tile import *
-from Deck import *
+from Deck import Deck
 from Player import *
 from Rule import *
 
@@ -7,19 +7,20 @@ from Rule import *
 # Game controller
 class Controller:
     Players: dict[WIND, Player] = {}
-    Deck: Deck = None
+    DeckRef: Deck = None
+    RoundWind: WIND = WIND.EAST # 局風位
     CurrentWind: WIND = WIND.EAST # 當前回合的玩家
     DealerWind: WIND = WIND.EAST # 莊家風位
     RoundNum: int = 0 # 局數 (東南西北)
     GameNum: int = 0 # 第幾莊
 
     def __init__(self):
-        self.Deck = Deck()
+        self.DeckRef = Deck()
         self.Players = {
-            WIND.EAST: Player('小東', WIND.EAST, self.Deck), 
-            WIND.SOUTH: Player('小南', WIND.SOUTH, self.Deck), 
-            WIND.WEST: Player('小西', WIND.WEST, self.Deck), 
-            WIND.NORTH: Player('小北', WIND.NORTH, self.Deck)
+            WIND.EAST: Player('小東', WIND.EAST, self.DeckRef), 
+            WIND.SOUTH: Player('小南', WIND.SOUTH, self.DeckRef), 
+            WIND.WEST: Player('小西', WIND.WEST, self.DeckRef), 
+            WIND.NORTH: Player('小北', WIND.NORTH, self.DeckRef)
         }
         for player in self.Players.values():
             player.daemon = True
@@ -30,32 +31,46 @@ class Controller:
         all_tiles = []
         for player in self.Players.values():
             all_tiles.extend(player.ReturnAllTile())
-        all_tiles.extend(self.Deck.Wall)
-        all_tiles.extend(self.Deck.DeadWall)
-        for discard_pile in self.Deck.Discard.values():
+        all_tiles.extend(self.DeckRef.Wall)
+        all_tiles.extend(self.DeckRef.DeadWall)
+        for discard_pile in self.DeckRef.Discard.values():
             all_tiles.extend(discard_pile)
-        self.Deck.Tiles = all_tiles
-        self.Deck.FlushTiles({}) # 清空所有牌堆
+        self.DeckRef.Tiles = all_tiles
+        self.DeckRef.FlushTiles({}) # 清空所有牌堆
 
         # 重置玩家狀態
         for player in self.Players.values():
             player.Reset()
 
         # 重置牌堆
-        self.Deck = Deck()
+        self.DeckRef = Deck()
         self.CurrentWind = self.DealerWind # 回合從莊家開始
 
     def StartGame(self):
         # 1. 洗牌、切牌
         diceScore = random.randint(3,18)
-        self.Deck.BreakingWall(self.DealerWind, diceScore)
+        self.DeckRef.BreakingWall(self.DealerWind, diceScore)
 
         # 2. 發牌
         handTiles = {WIND.EAST:[], WIND.SOUTH:[], WIND.WEST:[], WIND.NORTH:[]}
-        self.Deck.DealTiles(handTiles)
+        self.DeckRef.DealTiles(handTiles)
         for key,val in handTiles.items():
             self.Players[key].SetHandTile(val)
 
+    def StartRound(self):
+        pass
+
+    def EndRound(self):
+        pass
+
+    # 處理動作優先級 (吃/碰/槓/胡)
+    def DecideWhoseTurn(self) -> WIND:
+        pass
+
+
 
 if __name__ == '__main__':
-    pass
+    
+    ctrl = Controller()
+    ctrl.StartGame()
+    PrintLog('Finish')    
